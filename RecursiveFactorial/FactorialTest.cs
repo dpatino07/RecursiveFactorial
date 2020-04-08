6a0d9c46bc451cb59b9096db2afea73c7ad23449
Dominique Patino <DPatin2@wgu.com>
2020-04-08 13:09:22 -0700

    Add project files.

---
+++ RecursiveFactorial/FactorialTest.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace RecursiveFactorial
+{
+    class FactorialTest
+    {
+        static void Main()
+        {
+            // calculate the factorials of 0 through 10
+            for (long counter = 0; counter <= 15; ++counter)
+            {
+                Console.WriteLine($"{counter}! = {Factorial(counter)}");
+            }
+        }
+
+        // recursive declaration of method Factorial
+        static long Factorial(long number)
+        {
+            // base case
+            if (number <= 1)
+            {
+                return 1;
+            }
+            else // recursion step
+            {
+                return number * Factorial(number - 1);
+            }
+        }
+    }
+}
